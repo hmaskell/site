d59190d0081e607d7e46edf0cbf9ad39a6217a4f
Heather <hmaskell@sfu.ca>
2020-11-23 01:45:51 -0800

    added main page sections and placeholder content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+var hero_icons_plugin = require("tailwind-heropatterns")({
+  // as per tailwind docs you can pass variants
+  variants: [],
+
+  // the list of patterns you want to generate a class for
+  // the names must be in kebab-case
+  // an empty array will generate all 87 patterns
+  patterns: ["wiggle", "plus", "rain", "hexagons", "volcano-lamp", "bank-note", "aztec", "endless-clouds"],
+
+  // The foreground colors of the pattern
+  colors: {
+    default: "#D6BCFA",
+  },
+
+  // The foreground opacity
+  opacity: {
+    default: ".05",
+  }
+});
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -9,12 +29,17 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    hero_icons_plugin
+  ],
 
   theme: {
     fontFamily: {
-
-     'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+      'serif': ['Fredoka One', defaultTheme.fontFamily.serif],
+      'sans': ['Open Sans', defaultTheme.fontFamily.sans]
     }
   }
 }
+
+
+  ;
