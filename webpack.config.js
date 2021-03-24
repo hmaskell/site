4d6e628db6ef2e510e3481e1be35f65821fd5827
Heather <hmaskell@sfu.ca>
2021-03-23 23:39:38 -0700

    styling updates to match v3 figma

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|svg|jpg)$/,
+                test: /\.(png|svg|jpg|gif)$/,
                 type: 'asset/resource',
             },
             {
